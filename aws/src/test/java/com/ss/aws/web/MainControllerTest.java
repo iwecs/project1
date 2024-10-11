7a8a0890993760213b2b4292a78acb40b3bb6587
iwecs <bjsoon3333@gmail.com>
2024-10-11 11:13:29 +0900

    1

--- aws/src/test/java/com/ss/aws/web/MainControllerTest.java
+++ aws/src/test/java/com/ss/aws/web/MainControllerTest.java
@@ -28,3 +28,21 @@ public class MainControllerTest {
 						resp.getBody());
 	}
 }
+
+/* 배포 
+ * 1. EC2 고정 ip 할당 받기
+ * 2. 모바텀으로 ssh 연결하기 
+ * 3. git EC2 (내 컴퓨터에 다운로드 되어있는지 확인)
+ *    git --version
+ *    git 버전이 나올 시에는 설치 안해도됨!
+ *    만약 버전이 안나오면!
+ *    git 을 apt를 이용해서 git을 다운 받으면 됨
+ *    
+ *    apt 사용시 apt목록을 업데이트!
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
